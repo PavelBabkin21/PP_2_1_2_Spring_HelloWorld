3d41ab5bb8a8878b000bb9eaf7c62aa56bc947fa
PavelBabkin <pavel.babkin21@gmail.com>
2022-07-06 18:33:26 +0600

    PP_2_1_2_Spring_HelloWorld_Version_2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,10 +3,24 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
+
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+
+        HelloWorld beanWorld = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(beanWorld.getMessage());
+        HelloWorld beanWorld2 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(beanWorld2.getMessage());
+
+        Cat beanCat = (Cat) applicationContext.getBean("Cat");
+        System.out.println(beanCat.getMessage());
+        Cat beanCat2 = (Cat) applicationContext.getBean("Cat");
+        System.out.println(beanCat2.getMessage());
+
+        boolean beanTestWorld = beanWorld == beanWorld2;
+        boolean beanTestCat = beanCat == beanCat2;
+
+        System.out.println(beanTestWorld);
+        System.out.println(beanTestCat);
     }
 }
